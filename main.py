f1a439148dc7294b281b8b5af8b0f5cf43d4e252
LEcombi <combi123@outlook.de>
2025-03-15 23:15:43 +0100

    Beta-Version 0.0.2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import random
+from turtledemo.chaos import jumpto
+import time
 
 Number = random.randrange(1, 100)
 lemonade_Buying_Price = 0.89
+Rainy = 0.76
 
 usury_price_1 = 3.50
 usury_price_2 = 4.50
@@ -9,6 +12,7 @@ usury_price_3 = 5.50
 Normal_Price = 2.50
 
 RED = "\033[31m"
+BLUE = "\033[34m"
 RESET = "\033[0m"
 
 if Number > 50 or Number == 50:
@@ -16,34 +20,45 @@ if Number > 50 or Number == 50:
 elif Number < 50:                 #Set the weather
     Weather = "Rainy"
 
-print(f"The weather is {Weather} today.")
-print(f"One lemonade costs you {RED}{lemonade_Buying_Price}{RESET}.")
-lemonade_Selling_Price = float(input("How much do you want to sell your Lemon for: "))
+while True:
+
+    print(f"The weather is {Weather} today.")
+    print(f"One lemonade costs you {RED}{lemonade_Buying_Price}{RESET}.")
+    lemonade_Selling_Price = float(input("How much do you want to sell your Lemon for: "))
 
 
-if lemonade_Selling_Price - lemonade_Buying_Price < Normal_Price:
-    customers = random.randrange(29, 129)
-    profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
-    print(profit)
+    if lemonade_Selling_Price - lemonade_Buying_Price < Normal_Price:
+        customers = random.randrange(29, 129)
+        profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
 
-elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_1:
-    customers = random.randrange(19, 98)
-    profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
-    print(profit)
 
-elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_2:
-    customers = random.randrange(13, 68)
-    profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
-    print(profit)
+    elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_1:
+        customers = random.randrange(19, 98)
+        profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
 
-elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_3:
-    customers = random.randrange(9, 48)
-    profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
-    print(profit)
-else:
-    customers = random.randrange(3, 28)
-    profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
-    print(profit)
+
+    elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_2:
+        customers = random.randrange(13, 68)
+        profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
+
+
+    elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_3:
+        customers = random.randrange(9, 48)
+        profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
+
+    else:
+        customers = random.randrange(1, 8)
+        profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
+
+    if Weather == "Rainy":
+        profit = profit * Rainy
+
+    print("--------------------------------")
+    print(f"You sold {BLUE}{customers}{RESET} lemonades")
+    print(f"Your profit is {BLUE}{profit}{RESET}")
+    print("--------------------------------")
+    time.sleep(2)
+
 
 
 
