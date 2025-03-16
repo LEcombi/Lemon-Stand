b2469f36f1ca68325ae7acb668d2ff80e5348ea7
LEcombi <combi123@outlook.de>
2025-03-16 11:40:18 +0100

    Beta-Version 1.0.2

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import random
-from turtledemo.chaos import jumpto
 import time
+import subprocess
 
 Number = random.randrange(1, 100)
 lemonade_Buying_Price = 0.89
 Rainy = 0.76
+Level = 1
+New_Price = "False"
+
+if Level == 1:
+    Money = 100
 
 usury_price_1 = 3.50
 usury_price_2 = 4.50
@@ -17,30 +22,52 @@ RESET = "\033[0m"
 
 if Number > 50 or Number == 50:
     Weather = "Sunny"
-elif Number < 50:                 #Set the weather
+elif Number < 50:
     Weather = "Rainy"
 
 while True:
+    Number2 = random.randrange(0, 10)
+    if Number2 < 1:
+        lemonade_Buying_Price *= 2
+        New_Price = "True"
 
-    print(f"The weather is {Weather} today.")
+    if New_Price == "True":
+        print("The price of the lemonade has changed!")
+        print(f"The new price is {BLUE}{lemonade_Buying_Price}{RESET}")
+        New_Price = "False"
+    print(f"-------------------{RED}Conditions{RESET}---------------------")
+    print(f"The weather is {BLUE}{Weather}{RESET} today.")
+    print(f"You have {BLUE}{Money}{RESET}€")
     print(f"One lemonade costs you {RED}{lemonade_Buying_Price}{RESET}.")
-    lemonade_Selling_Price = float(input("How much do you want to sell your Lemon for: "))
 
+    Purchasable_float = Money / lemonade_Buying_Price
+    Purchasable = int(Purchasable_float)
+
+    print(f"You can buy {BLUE}{Purchasable}{RESET} lemonades.")
+    print("--------------------------------------------------")
+
+    lemonade_Buying_Crowd = int(input("How many lemonades do you want to buy: "))
+
+    total_cost = lemonade_Buying_Crowd * lemonade_Buying_Price
+    if total_cost > Money:
+        print(f"Sorry, you don't have enough money to buy {lemonade_Buying_Crowd} lemonades.")
+        continue
+
+    Money -= total_cost
+
+    lemonade_Selling_Price = float(input("How much do you want to sell your Lemon for: "))
 
     if lemonade_Selling_Price - lemonade_Buying_Price < Normal_Price:
         customers = random.randrange(29, 129)
         profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
 
-
     elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_1:
         customers = random.randrange(19, 98)
         profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
 
-
     elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_2:
         customers = random.randrange(13, 68)
         profit = (float(lemonade_Selling_Price) - lemonade_Buying_Price) * customers
-
 
     elif lemonade_Selling_Price - lemonade_Buying_Price < usury_price_3:
         customers = random.randrange(9, 48)
@@ -53,12 +80,14 @@ while True:
     if Weather == "Rainy":
         profit = profit * Rainy
 
-    print("--------------------------------")
+    print(f"------------------{RED}Invoice{RESET}-------------------")
     print(f"You sold {BLUE}{customers}{RESET} lemonades")
     print(f"Your profit is {BLUE}{profit}{RESET}")
-    print("--------------------------------")
+    print(f"That was level {BLUE}{Level}{RESET}")
+    print(f"Your new account balance is {BLUE}{Money + profit}{RESET}")
+    print("--------------------------------------------")
+
+    New_Money = Money + profit
+    Level = Level + 1
+    Money = int(New_Money)
     time.sleep(2)
-
-
-
-
